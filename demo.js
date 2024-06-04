5748849ee931c9e8683e187cf9c53db99b00a5b3
Uddhav-singh <uddhav.singh.79@gmail.com>
2024-06-04 11:19:04 +0530

    created a server which runs on port 4000.

--- demo.js
+++ demo.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+// console.log("Hello");
+
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Uddhav');
+  console.log('Uddhav');
+});
+
+server.listen(4000, () => {
+  console.log('Server is listening on port 4000');
+});
