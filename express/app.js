f293e9f25fd21df2647b250914df103b18d4f4a0
Uddhav-singh <uddhav.singh.79@gmail.com>
2024-06-05 13:24:39 +0530

    Added Initial Express JS.

---
+++ express/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req,res, next) => {
+    console.log("1st middleware!");
+    next();
+});
+
+app.use((req,res,next)=>{
+    console.log("2nd middleware!");
+    res.send('<h1>Hello! from Express.js</h1>')
+});
+
+app.listen(3000);
