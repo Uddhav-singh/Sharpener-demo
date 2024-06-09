80bfd5d0dd9b80b9eb3085128a158e45d8eb65ac
Uddhav-singh <uddhav.singh.79@gmail.com>
2024-06-09 11:24:20 +0530

    Added middleware and route /add-product with two input field named title and size

--- express/app.js
+++ express/app.js
@@ -1,15 +1,35 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req,res, next) => {
-    console.log("1st middleware!");
-    next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use((req, res, next) => {
+//   console.log("1st middleware!");
+//   next();
+// });
+
+app.get('/add-product',(req, res, next) => {
+  console.log("1st middleware!");
+  res.send(`
+    <form action="/product" method="POST">
+        <input type="text" name="title">
+        <input type="text" name="size">
+        <button type="submit">Add Product</button>
+    </form>
+`);
 });
 
-app.use((req,res,next)=>{
-    console.log("2nd middleware!");
-    res.send('<h1>Hello! from Express.js</h1>')
+app.use('/product', (req, res, next) => {
+    const { title, size } = req.body;
+    console.log(`Title: ${title}, Size: ${size}`);
+    // console.log(req.body);
+    res.redirect('/');
 });
+
+app.use('/', (req,res,next)=>{
+    res.send("hello from express")
+})
 
 app.listen(3000);
