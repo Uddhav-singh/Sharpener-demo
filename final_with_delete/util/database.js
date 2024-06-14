e344e2577f5fda66226af5438ddc2a31ac963271
Uddhav-singh <uddhav.singh.79@gmail.com>
2024-06-14 16:58:24 +0530

    implemented mysql connection.

--- final_with_delete/util/database.js
+++ final_with_delete/util/database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host:'localhost',
     user: 'root',
-    database: 'node-complete',
-    password: 'rootpassword'
+    password: 'rootpassword',
+    database: 'node-complete'
 });
 
 module.exports = pool.promise();
