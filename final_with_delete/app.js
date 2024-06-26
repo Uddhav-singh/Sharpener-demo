357d6be8198bd29518965661ad4013a3cc039ae4
Uddhav-singh <uddhav.singh.79@gmail.com>
2024-06-26 17:34:05 +0530

    added cart and cart-item model with delete functionality.

--- final_with_delete/app.js
+++ final_with_delete/app.js
@@ -1,64 +1,67 @@
-const path = require("path");
-const sequelize = require("./util/database");
-const express = require("express");
-const bodyParser = require("body-parser");
-const errorController = require("./controllers/error");
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-
-// db.execute('SELECT * FROM products')
-// .then(result => {
-//     console.log(result)
-// })
-// .catch(err => {
-//     console.log(err);
-// });
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
   User.findByPk(1)
-  .then(user=>{
-    req.user = user;
-    next();
-  }).catch(err => console.log(err))
-})
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
-app.use("/admin", adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  // .sync({force: true})
+  // .sync({ force: true })
   .sync()
-  .then((result) => {
+  .then(result => {
     return User.findByPk(1);
     // console.log(result);
-    // app.listen(3000);
   })
-  .then(user=> {
-    if(!user){
-    return User.create({name: 'max', email: 'test@test.com'});
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Max', email: 'test@test.com' });
     }
     return user;
   })
-  .then(user=>{
-    console.log(user);
+  .then(user => {
+    // console.log(user);
+    return user.createCart();
+  })
+  .then(cart => {
     app.listen(3000);
   })
-  .catch((err) => {
+  .catch(err => {
     console.log(err);
   });
